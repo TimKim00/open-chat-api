222e1d18acf04b959a357fc0a2b20cb32956ea8d
Yeonsu Kim <yeonsukim@yeonsus-mbp.mynetworksettings.com>
2023-07-25 16:20:03 -0700

    naming

--- user-service/src/config/config.js
+++ user-service/src/config/config.js
@@ -7,15 +7,22 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 // get config values from process.env
+let config = {
+host: process.env.PG_HOST,
+user: process.env.PG_USER,
+password: process.env.PG_PASSWORD,
+database: process.env.PG_DATABASE,
+port: process.env.PG_PORT
+}
+
+if (process.env.NODE_ENV === 'test') {
+  config.host = process.env.DB_HOST;
+  config.user = process.env.DB_USER;
+  config.password = process.env.DB_PASS;
+} 
 
 module.exports = {
-  pg: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT
-  },
+  pg: config,
   mongodb: {
     uri: process.env.MONGODB_URI
   },
