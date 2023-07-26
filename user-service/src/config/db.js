b90e5ced1c24f38895c786a4fe0089df0653b7d2
Yeonsu Kim <yeonsukim@yeonsus-mbp.mynetworksettings.com>
2023-07-25 17:49:37 -0700

    diff commit

--- user-service/src/config/db.js
+++ user-service/src/config/db.js
@@ -19,6 +19,10 @@ if (process.env.NODE_ENV !== "test") {
   });
 } else {
   module.exports = new Pool({
-    connectionString
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.PORT
   })
 }
