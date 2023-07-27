67858ca9b4c81446d18147a75fcd6ab6c02ccdd9
Yeonsu Kim <yeonsukim@yeonsus-mbp.mynetworksettings.com>
2023-07-26 22:06:51 -0700

    first-try

--- user-service/tests/unit_tests.js
+++ user-service/tests/unit_tests.js
@@ -17,7 +17,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 /** Start the mock application.  */
-const server = process.env.SERVER_ADDRESS;
+// const server = process.env.SERVER_ADDRESS;
+const server = require("../server");
 
 chai.should();
 chai.use(chaiHttp);
