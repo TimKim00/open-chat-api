3597fa50e6a3a44dc5bd716a41d311bff895dfdd
Yeonsu Kim <yeonsukim@yeonsus-mbp.mynetworksettings.com>
2023-07-26 17:40:52 -0700

    notification started

---
+++ notification-service/app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+const { sendEmail } = require('./mailer');
+
+const app = express();
+app.use(express.json());
+
+app.post('/send-notification', async (req, res) => {
+  const { to, subject, message } = req.body;
+  try {
+    await sendEmail(to, subject, message);
+    res.status(200).send('Notification sent');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error sending notification');
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Notification Service is listening on port ${port}`));
